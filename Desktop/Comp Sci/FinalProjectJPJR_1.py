a088ca3d151bca3750024210c63f3c30fd920963
NOKK-SH <tus17052@temple.edu>
2024-12-12 16:38:09 -0500

    small change

--- Desktop/Comp Sci/FinalProjectJPJR_1.py
+++ Desktop/Comp Sci/FinalProjectJPJR_1.py
@@ -6,7 +6,7 @@ WIDTH = 1000
 HEIGHT = 600
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 surface = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
-pygame.display.set_caption('Jetpack Joyride Remake in Python!')
+pygame.display.set_caption('RocketRider!')
 fps = 60
 timer = pygame.time.Clock()
 font = pygame.font.Font('freesansbold.ttf', 32)
